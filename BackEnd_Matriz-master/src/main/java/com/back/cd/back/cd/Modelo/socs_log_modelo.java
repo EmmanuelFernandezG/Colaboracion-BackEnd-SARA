124e66a77e7e755a0cf0b2c70cdf190a358eabc6
Sara-Truper <srodriguezg@truper.com>
2026-04-23 13:10:05 -0600

    23 04 2026

--- BackEnd_Matriz-master/src/main/java/com/back/cd/back/cd/Modelo/socs_log_modelo.java
+++ BackEnd_Matriz-master/src/main/java/com/back/cd/back/cd/Modelo/socs_log_modelo.java
@@ -32,64 +32,97 @@ public class socs_log_modelo {
     private String comentarios_reimp;
 	@Column(name = "status_reimp")
     private String status_reimp;
+
+	@Column(name = "asistentepos")
+    private String asistentepos;
+
 	public Long getNopo() {
 		return nopo;
 	}
+
 	public void setNopo(Long nopo) {
 		this.nopo = nopo;
 	}
+
 	public LocalDate getAutorizacion_previa() {
 		return autorizacion_previa;
 	}
+
 	public void setAutorizacion_previa(LocalDate autorizacion_previa) {
 		this.autorizacion_previa = autorizacion_previa;
 	}
+
 	public String getComentarios_doc() {
 		return comentarios_doc;
 	}
+
 	public void setComentarios_doc(String comentarios_doc) {
 		this.comentarios_doc = comentarios_doc;
 	}
+
 	public LocalDate getFecha_final_plan() {
 		return fecha_final_plan;
 	}
+
 	public void setFecha_final_plan(LocalDate fecha_final_plan) {
 		this.fecha_final_plan = fecha_final_plan;
 	}
+
 	public String getComentarios_plan() {
 		return comentarios_plan;
 	}
+
 	public void setComentarios_plan(String comentarios_plan) {
 		this.comentarios_plan = comentarios_plan;
 	}
+
 	public LocalDate getFecha_final_compras() {
 		return fecha_final_compras;
 	}
+
 	public void setFecha_final_compras(LocalDate fecha_final_compras) {
 		this.fecha_final_compras = fecha_final_compras;
 	}
+
 	public String getComentarios_compras() {
 		return comentarios_compras;
 	}
+
 	public void setComentarios_compras(String comentarios_compras) {
 		this.comentarios_compras = comentarios_compras;
 	}
+
 	public String getNumero_reimp() {
 		return numero_reimp;
 	}
+
 	public void setNumero_reimp(String numero_reimp) {
 		this.numero_reimp = numero_reimp;
 	}
+
 	public String getComentarios_reimp() {
 		return comentarios_reimp;
 	}
+
 	public void setComentarios_reimp(String comentarios_reimp) {
 		this.comentarios_reimp = comentarios_reimp;
 	}
+
 	public String getStatus_reimp() {
 		return status_reimp;
 	}
+
 	public void setStatus_reimp(String status_reimp) {
 		this.status_reimp = status_reimp;
 	}
+
+	public String getAsistentepos() {
+		return asistentepos;
+	}
+
+	public void setAsistentepos(String asistentepos) {
+		this.asistentepos = asistentepos;
+	}
+
+
 }
